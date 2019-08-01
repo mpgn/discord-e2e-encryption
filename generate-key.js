7f294a0491e41556b012132ef0b0a8d94640a8ee
kadogo <kadogo@users.noreply.github.com>
2019-08-01 12:16:05 +0200

    Use deriveKey for make a key with pbdkf2 also let the user using passphrase and salt directly in keystorage

--- generate-key.js
+++ generate-key.js
@@ -2,35 +2,29 @@
     This script should be only run into the dev console
     More information: https://github.com/mpgn/discord-e2e-encryption
 */
-
-async function generateKey(password, salt, iterations) {
-    // Based on https://8gwifi.org/docs/window-crypto-pbkdf.jsp
-
-    // Define the different part for the key
-    let saltEncoder = new TextEncoder('utf-8')
-    let saltBuffer = saltEncoder.encode(salt)
-    let encoder = new TextEncoder('utf-8')
-    let passphraseKey = encoder.encode(password)
-
-    let key = await window.crypto.subtle.importKey(
-        'raw',
-        passphraseKey,
-        {name: 'PBKDF2'},
+async function generateKey(passphrase, salt){
+    // Import password as CryptoKey object
+    let enc = new TextEncoder()
+    let masterkey = await window.crypto.subtle.importKey(
+        "raw",
+        enc.encode(passphrase),
+        {name: "PBKDF2"},
         false,
-        ['deriveBits', 'deriveKey']
+        ["deriveBits", "deriveKey"]
     )
 
     return window.crypto.subtle.deriveKey(
-        { "name": 'PBKDF2',
-          "salt": saltBuffer,
-          "iterations": iterations,
-          "hash": 'SHA-256'
-        },
-        key,
-        { "name": 'AES-CBC', "length": 256 },
-        true,
-        [ "encrypt", "decrypt" ]
-    )
+      {
+        "name": "PBKDF2",
+        salt: enc.encode(salt),
+        "iterations": 100000,
+        "hash": "SHA-256"
+      },
+      masterkey,
+      { "name": "AES-GCM", "length": 256},
+      false,
+      [ "encrypt", "decrypt" ]
+    );
 }
 
 async function exportKey(key) {
@@ -40,7 +34,7 @@ async function exportKey(key) {
     )
 }
 
-var key = await generateKey("your password", "your salt", 10000)
+var key = await generateKey("your password", "your salt")
 exportKey(key).then(function (result) {
     console.log(result)
 })
